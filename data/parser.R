ad7c57829a81dff09fc99b263172b5027edae100
Joel Ricci-Lopez <ricci.lob@gmail.com>
2020-03-29 00:02:31 -0700

    Deceased and Suspect cases added

--- data/parser.R
+++ data/parser.R
@@ -17,6 +17,8 @@ preprocessing_data <- function() {
   
   # ******** Daily Reports per State ********
   ord_state_names <- data_list$ord_state_names
+  
+  # Positive Cases
   df_pos_states <- raw_daily_data[, -grep("\\_.*$", colnames(raw_daily_data))]
   # Remove unusefull columns
   df_pos_states[, c('Fecha', 'Pos', 'Recovered', 'Deceased', 'Susp')] <- NULL
@@ -24,9 +26,29 @@ preprocessing_data <- function() {
   colnames(df_pos_states) <- ord_state_names
   df_pos_states <- df_pos_states[ , order(names(df_pos_states))]
   
+  # Suspect Cases
+  df_sup_states <- raw_daily_data[, grep("\\_S$", colnames(raw_daily_data))]
+  # Remove unusefull columns
+  df_sup_states[, c('Fecha', 'Pos', 'Recovered', 'Deceased', 'Susp')] <- NULL
+  # Order the state names to match the map ordering
+  colnames(df_sup_states) <- ord_state_names
+  df_sup_states <- df_sup_states[ , order(names(df_sup_states))]
+  
+  # Deceased
+  df_deceased_states <- raw_daily_data[, grep("\\_D$", colnames(raw_daily_data))]
+  # Remove unusefull columns
+  df_deceased_states[, c('Fecha', 'Pos', 'Recovered', 'Deceased', 'Susp')] <- NULL
+  # Order the state names to match the map ordering
+  colnames(df_deceased_states) <- ord_state_names
+  df_deceased_states <- df_deceased_states[ , order(names(df_deceased_states))]
+  
   # ******** Current Dates ********
   # Get the number of maximum cases of any category for TODAY
-  max_today <- apply(df_pos_states, 2, max, na.rm = TRUE)
+  max_pos_today <- apply(df_pos_states, 2, max, na.rm = TRUE)
+  # Max suspect today
+  max_sup_today <- apply(df_sup_states, 2, max, na.rm = TRUE)
+  # Max suspect today
+  max_deceased_today <- apply(df_deceased_states, 2, max, na.rm = TRUE)
   # First date
   first_date <- head(raw_daily_data$Fecha, n = 1)[1]
   # Last date
@@ -39,7 +61,11 @@ preprocessing_data <- function() {
   parser <- list(
     df_total_reps = df_total_reps,
     df_pos_states = df_pos_states,
-    max_today = max_today,
+    df_sup_states = df_sup_states,
+    df_deceased_states = df_deceased_states,
+    max_pos_today = max_pos_today,
+    max_sup_today = max_sup_today,
+    max_deceased_today = max_deceased_today,
     first_date = first_date,
     last_date = last_date,
     mexico = mexico
